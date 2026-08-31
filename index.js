ff3aefb6524a90dbbc835add53c89668642fa129
jrentiress-cyber <jrentiress@gmail.com>
2026-08-31 21:59:21 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => res.send('Bot is Alive 24/7!'));
+app.listen(3000, () => console.log('Server is running!'));
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    
+    // Auto Join Voice Channel
+    const guild = client.guilds.cache.get(process.env.GUILD_ID);
+    if (guild) {
+        joinVoiceChannel({
+            channelId: process.env.CHANNEL_ID,
+            guildId: guild.id,
+            adapterCreator: guild.voiceAdapterCreator,
+            selfDeaf: false,
+            selfMute: false
+        });
+        console.log("Joined VC successfully!");
+    }
+});
+
+client.login(process.env.TOKEN);
