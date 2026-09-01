1360cb6722fee4525e40d0cb693c2c56073796f6
jrentiress-cyber <jrentiress@gmail.com>
2026-09-01 13:37:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
 const express = require('express');
 
 const app = express();
@@ -13,6 +13,8 @@ const client = new Client({
     ]
 });
 
+let globalConnection = null;
+
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
@@ -24,19 +26,41 @@ client.once('ready', () => {
         if (guild) {
             const channel = guild.channels.cache.get(channelId);
             if (channel) {
-                joinVoiceChannel({
+                globalConnection = joinVoiceChannel({
                     channelId: channel.id,
                     guildId: guild.id,
                     adapterCreator: guild.voiceAdapterCreator,
                     selfDeaf: false,
-                    selfMute: false
+                    selfMute: false,
                 });
-                console.log("Successfully connected to Voice Channel and staying stable!");
+                console.log("Bot successfully connected to the fixed voice channel and staying stable!");
             } else {
                 console.log("Could not find the Voice Channel ID!");
             }
         } else {
             console.log("Could not find the Guild ID!");
+        }
+    }
+});
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+    const channelId = process.env.CHANNEL_ID;
+    const guildId = process.env.GUILD_ID;
+
+    if (newState.guild.id === guildId && newState.channelId === channelId) {
+        if (!oldState.channelId || oldState.channelId !== channelId) {
+            if (globalConnection) {
+                try {
+                    const player = createAudioPlayer();
+                    const resource = createAudioResource('./radhe.mp3');
+
+                    globalConnection.subscribe(player);
+                    player.play(resource);
+                    console.log("Member joined the fixed channel, playing Radhe Radhe!");
+                } catch (err) {
+                    console.error("Error playing audio:", err);
+                }
+            }
         }
     }
 });
